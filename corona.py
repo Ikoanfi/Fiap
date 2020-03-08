5d47eee9a60f2c5d3a8da96d51d10d773f9ec329
DanielNYFiap <61586722+DanielNYFiap@users.noreply.github.com>
2020-03-08 17:49:10 -0300

    Add files via upload

---
+++ corona.py
@@ -0,0 +1,13 @@
+nome = input("Nome do paciente: ")
+idade = int(input("Idade do paciente: "))
+risco = input("Eh grupo de risco? (sim ou nao) ").upper()
+
+if risco != "SIM" and risco != "NAO":
+    risco = input("Entre somente com sim ou nao ").upper()
+else:
+    if idade < 15 or idade > 60:
+        print("Paciente: ", nome.title(), " prioridade")
+    elif risco == "SIM":
+        print("Paciente: ", nome.title(), " prioridade")
+    else:
+        print("Paciente: ", nome.title(), " segue na fila")
